bf1de83c0e5e07f8d4d1e53fa48b0d071a161f59
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-02 20:58:21 +0300

    routes/auth.js create new user added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
+const User = require("../models/User");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("auth page");
+router.post("/register", async (req, res) => {
+  //create new user
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.username,
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
