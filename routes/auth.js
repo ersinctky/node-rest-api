f644033546d135793dd0b5c5d262df86cc320498
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-02 22:22:10 +0300

    route/auth.js bcrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,22 @@
 const User = require("../models/User");
-
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
-  //create new user
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.username,
-  });
-
   try {
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
