daed5457496322fcc88c75998d62806aa3dc1e1f
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-02 18:08:21 +0300

    helpers folder created & mongoDB connection successful

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const connectDatabase = require("./helpers/database/connectDatabase");
+const path = require("path");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 
-dotenv.config();
+// environment variables
+
+dotenv.config({
+  path: "./config/.env",
+});
+
+// mongodb connection
+connectDatabase();
 
 app.listen(8800, () => {
   console.log("Backend server is running");
