892c83d29ee8097921f21f41c32de19bc3dbc2dc
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-02-07 15:42:32 +0300

    file uploading

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cors = require("cors");
+const multer = require("multer");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -23,10 +24,30 @@ app.use(cors());
 // mongodb connection
 connectDatabase();
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
